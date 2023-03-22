ae2ce2852ad3dfeb775a522c5501fc4d507181f7
DevBbang <ios_developer.th@icloud.com>
2023-03-22 10:56:23 +0900

    Quizzler Completed

---
+++ Udemy/Quizzler-iOS13/Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var firstChoice: UIButton!
+    @IBOutlet weak var secondChoice: UIButton!
+    @IBOutlet weak var thirdChoice: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        let userGotItRight = quizBrain.checkAnswer(userAnswer!)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        firstChoice.backgroundColor = .clear
+        secondChoice.backgroundColor = .clear
+        thirdChoice.backgroundColor = .clear
+        firstChoice.setTitle("\(quizBrain.getChoiceText(n: 0))", for: .normal)
+        secondChoice.setTitle("\(quizBrain.getChoiceText(n: 1))", for: .normal)
+        thirdChoice.setTitle("\(quizBrain.getChoiceText(n: 2))", for: .normal)
+    }
+}
+
