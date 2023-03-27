ec9ccb81da6339017b5442fe7c36cc7ddc2c9d05
DevBbang <ios_developer.th@icloud.com>
2023-03-27 10:47:06 +0900

    BMICalculator Completed

---
+++ Udemy/BMI-Calculator-iOS13/BMI Calculator/Views/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by TAEJANIM on 2023/03/27.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
---
+++ Udemy/BMI-Calculator-iOS13/BMI Calculator/Views/CalculatorBrain.swift
@@ -0,0 +1,39 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by TAEJANIM on 2023/03/27.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct CalculatorBrain {
+    
+    var bmi: BMI?
+    
+    func getBMIValue() -> String {
+        let bmiToOneDecimalPlace = String(format: "%.1f", bmi?.value ?? "0.0")
+        return bmiToOneDecimalPlace
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No advice"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? .black
+    }
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        let bmiValue = weight / (height * height)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: .blue)
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: .green)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: .red)
+        }
+    }
+}
