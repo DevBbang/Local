cf946ad999ab8bb513dcb823ffa63820968c6b50
DevBbang <ios_developer.th@icloud.com>
2023-04-05 08:44:40 +0900

    Dicee SwiftUI Completed

---
+++ Udemy/Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by TAEJANIM on 2023/04/05.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
