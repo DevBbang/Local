cfd0f7bf1c25ce87b7550421f34f0f1fdb0a6719
DevBbang <ios_developer.th@icloud.com>
2023-04-05 09:32:37 +0900

    H4X0R SwiftUI Completed

---
+++ Udemy/H4X0R News/H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by TAEJANIM on 2023/04/05.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
